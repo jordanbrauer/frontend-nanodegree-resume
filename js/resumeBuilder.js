72196fb49d01c570d731ab1778e4719b65861116
jordanbrauer <jbrauer.inc@gmail.com>
2016-11-26 00:42:44 -0600

    Finished bio and education display() functions.

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -11,42 +11,49 @@
 /*---------------------------------------------*\
   Create Resume Objects
 \*---------------------------------------------*/
+// placeholder reference variable
+var placeholder = '%data%';
 
 // biography resume object
 var bio = {
-  name: 'Jordan Brauer', // string
-  role: 'Front-end Engineer', // string
+  name: 'Jordan Brauer',
+  role: 'Front-end Engineer',
   contacts: {
     mobile: '204-781-4099',
+    location: 'Winnipeg, Canada',
     email: 'jbrauer.inc@gmail.com',
-    github: 'https://github.com/jordanbrauer',
     twitter: 'https://twitter.com/jordbrauer',
-    location: 'Winnipeg, Canada'
+    github: 'https://github.com/jordanbrauer'
   },
-  welcomeMessage: 'Creator, developer, and table-top gamer.',
+  welcomeMessage: `Hello, my name is Jordan. I am a publisher, creator, and table-top gaming enthusiast.`,
   skills: [
-    // languages
+    // languages/protocols
     'HTML5',
     'CSS3',
     'JavaScript',
     'SASS/SCSS',
     'PHP',
     'MySQL',
+    'Apache',
+    'ssh',
     // vcs
     'Git',
     // package managers
-    'yarn',
-    'npm',
+    'yarn', // no devicon
+    'npm', // no devicon
     'Bower',
-    'Composer',
+    'Composer', // no devicon
+    // task runners
+    'Gulp',
+    'Grunt',
     // frameworks
-    'Mocha.js',
-    'Chai.js',
+    'Mocha.js', // no devicon
+    'Chai.js', // no devicon
     'jQuery',
     'Twitter Bootstrap',
     'Foundation Sites',
-    'Foundation Emails',
-    'Smarty',
+    'Foundation Emails', // no devicon
+    'Smarty', // no devicon
     // editors
     'Atom Editor',
     'GIMP',
@@ -56,8 +63,83 @@ var bio = {
     'Mac OS X',
     'Linux'
   ],
-  biopic: 'string(url)',
-  display: function() { }
+  biopic: 'https://avatars3.githubusercontent.com/u/18744334?v=3&u=c575c5b70e692078d8543a10028f2f4692f0003d&s=400',
+  display: function() {
+    // select container elements
+    var headerSection = $('#header');
+    var topContactsSection = $('#topContacts');
+    var footerContactsSection = $('#footerContacts');
+
+    // replace the %data% placeholders and save to a variable for later
+    var bioPic = HTMLbioPic.replace(placeholder, bio.biopic);
+    var bioName = HTMLheaderName.replace(placeholder, bio.name);
+    var bioRole = HTMLheaderRole.replace(placeholder, bio.role);
+    var bioWelcomeMsg = HTMLwelcomeMsg.replace(placeholder, bio.welcomeMessage);
+    var bioContactEmail = HTMLemail.replace(placeholder, bio.contacts.email);
+    var bioContactMobile = HTMLmobile.replace(placeholder, bio.contacts.mobile);
+    var bioContactGitHub = HTMLgithub.replace(placeholder, bio.contacts.github);
+    var bioContactTwitter = HTMLtwitter.replace(placeholder, bio.contacts.twitter);
+    var bioContactLocation = HTMLlocation.replace(placeholder, bio.contacts.location);
+
+    // display the bio picture, name, role, and welcome message.
+    headerSection
+      .append(bioPic)
+      .append(bioName)
+      .append(bioRole)
+      .append(bioWelcomeMsg);
+
+    // display the contact information in the header
+    topContactsSection
+      .append(bioContactEmail)
+      .append(bioContactMobile)
+      .append(bioContactGitHub)
+      .append(bioContactTwitter)
+      .append(bioContactLocation);
+
+    // display the contact information in the footer
+    footerContactsSection
+      .append(bioContactEmail)
+      .append(bioContactMobile)
+      .append(bioContactGitHub)
+      .append(bioContactTwitter)
+      .append(bioContactLocation);
+
+    // check to see if the bio has a skills section with atleast 1 entry
+    // if so, display any and all entries.
+    if (bio.skills.length > 0) {
+      // start the skills section
+      headerSection.append(HTMLskillsStart);
+
+      // select the skills section
+      var skillsSection = $('#skills');
+
+      // displaySkill()
+      // create an image icon for a skill as a nice way to visually display them
+      var displaySkill = function (skillStr) {
+        // format the skill to remove any "illegal" characters
+        var formattedSkill =
+          skillStr.toLowerCase()
+            .replace(' ', '-')
+            .replace('.', '-')
+            .replace('/', '-');
+
+        // create an image tag using the formatted skill as an image name
+        // use the skill parameter passed to the function as the alt text for accessibility.
+        var skillImage = `<img src="./assets/img/logo-${formattedSkill}.svg" alt="${skillStr}">`;
+
+        // replace the placeholder with the skillImage
+        var skillData = HTMLskills.replace(placeholder, skillImage);
+
+        // render the skill to the page
+        skillsSection.append(skillData);
+      };
+
+      // loop through the bio.skills array and invoke displaySkill() for each index.
+      for (var i = 0; i < bio.skills.length; i++) {
+        displaySkill(bio.skills[i]);
+      }
+    }
+  }
 };
 
 // education resume object
@@ -69,7 +151,7 @@ var education = {
       degree: 'New Media Design Certificate',
       majors: [
         'Front-end Development',
-        'Business'
+        'Relational Database Design'
       ],
       dates: 'September 2013 - June 2014',
       url: 'https://www.lrsd.net/schools/ATC/Pages/default.aspx'
@@ -88,13 +170,87 @@ var education = {
   ],
   onlineCourses: [
     {
-      title: 'Front-end Nanodgree',
+      title: 'Front-end Web Developer Nanodgree',
       school: 'Udacity',
-      dates: 'string - string',
-      url: 'string'
+      dates: 'September 2016 - Present',
+      url: 'https://www.udacity.com/course/front-end-web-developer-nanodegree--nd001?v=fe1'
     }
   ],
-  display: function () { }
+  display: function () {
+    // select the container elements
+    var educationSection = $('#education');
+
+    // displaySchool()
+    // create an education block on the page from an object
+    var displaySchool = function(educationObj) {
+      // start a new education block
+      educationSection.append(HTMLschoolStart);
+
+      // last entry in the array
+      var educationEntry = $('.education-entry:last');
+
+      // replace the %data% placeholders and save to a variable for later
+      var schoolName =
+        HTMLschoolName
+          .replace(placeholder, educationObj.name)
+          .replace('#', educationObj.url);
+
+      var schoolDegree = HTMLschoolDegree.replace(placeholder, educationObj.degree);
+      var schoolDates = HTMLschoolDates.replace(placeholder, educationObj.dates);
+      var schoolLocation = HTMLschoolLocation.replace(placeholder, educationObj.location);
+      var schoolMajor = HTMLschoolMajor.replace(placeholder, educationObj.majors);
+
+      // render the education block to the page.
+      educationEntry
+        .append(schoolName + schoolDegree)
+        .append(schoolDates)
+        .append(schoolLocation)
+        .append(schoolMajor);
+    };
+
+    // check to see if the education.schools array has any entries
+    // if so, display any and all schools.
+    if (education.schools.length > 0) {
+      // loop through the schools array and invoke displaySchool() for each index
+      for (var i = 0; i < education.schools.length; i++) {
+        displaySchool(education.schools[i]);
+      }
+    }
+
+    // displayCourse()
+    // create an education block on the page from an object
+    var displayCourse = function(courseObj) {
+      educationSection.append(HTMLschoolStart);
+
+      var educationEntry = $('.education-entry:last');
+
+      var onlineTitle =
+        HTMLonlineTitle
+          .replace(placeholder, courseObj.title)
+          .replace('#', courseObj.url);
+
+      var onlineSchool = HTMLonlineSchool.replace(placeholder, courseObj.school);
+      var onlineDates = HTMLonlineDates.replace(placeholder, courseObj.dates);
+      // var onlineUrl = HTMLonlineURL.replace(placeholder, courseObj.url);
+
+      educationEntry
+        .append(onlineTitle + onlineSchool)
+        .append(onlineDates)
+        // .append(online);
+    };
+
+    // check to see if the education.onlineCourses array has any entires
+    // if so, display any and all courses
+    if (education.onlineCourses.length > 0) {
+      // display the header
+      educationSection.append(HTMLonlineClasses);
+
+      // loop through the online courses array and invoke displayCourse() for each index
+      for (var i = 0; i < education.onlineCourses.length; i++) {
+        displayCourse(education.onlineCourses[i]);
+      }
+    }
+  }
 };
 
 // work experience resume object
@@ -105,7 +261,11 @@ var work = {
       title: 'Web Developer',
       location: 'Winnipeg, Canada',
       dates: 'September 2014 - Present',
-      description: ''
+      description: `<ul>
+                      <li>• Develop and maintain the company website.</li>
+                      <li>• Design and print company  yers/ads.</li>
+                      <li>• Manage, maintain and promote the company Facebook, Twitter, and Instagram.</li>
+                    </ul>`
     },
     // {
     //   employer: 'string',
